4a5c950b34280d06cef5430d87061dc59903ec39
Adam Weil <adamjweil@gmail.com>
2018-02-14 11:41:17 -0500

    updating seed info

--- db/migrate/20171020182422_create_restaurants.rb
+++ db/migrate/20171020182422_create_restaurants.rb
@@ -5,8 +5,9 @@ class CreateRestaurants < ActiveRecord::Migration[5.1]
       t.string :cuisine, NULL: false
       t.string :address, NULL: false
       t.string :city, NULL: false
-      t.string :state, NULL: false
-      t.integer :zip, NULL: false
+      t.string :state
+      t.string :country, NULL: false
+      t.integer :zip
       t.integer :user_id, foreign_key: true, NULL: false
 
       t.timestamps(NULL: false)
